61975cd306e9cf1b1a6b9909ef8023b63dd0ed25
Temurjon Sharifov <temurjon.sharifov@gmail.com>
2020-10-01 22:51:50 +0500

    feat: solve 'Reverse Int' task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 // ! REVERSE INT
 
-module.exports = function reverse(n) {};
+module.exports = function reverse(n) {
+    n += "";
+    if (+n < 0) {
+        return n.slice(1).split("").reverse().join("");
+    } else {
+        return n.split("").reverse().join("");
+    }
+};
